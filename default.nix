7e792f77d6ab36bb179fe1a613df7fbf9f64f616
Chris De Leon <chrisdeleon333+dev@gmail.com>
2024-07-08 21:04:28 -0700

    fix permission errors

--- default.nix
+++ default.nix
@@ -9,30 +9,45 @@ let
     name = "dotfiles";
     src = ./configs;
     installPhase = ''
+      # Exits early if any command runs into an issue and prints commands as they are executed
       set -ex
+
+      # Copies source files into an output folder
       mkdir -p $out
       cp -rv $src/* $out
+
+      # Tells Nix to give bashrc executable permissions: https://stackoverflow.com/a/61736179
       chmod 777 $out/bashrc
+      
+      # Configures tmux to use an interactive non-login shell and source our custom bashrc file:
+      #  - https://discourse.nixos.org/t/interactive-bash-with-nix-develop-flake/15486
+      # - https://unix.stackexchange.com/a/98085
+      #  - https://unix.stackexchange.com/a/541352
+      #  - https://stackoverflow.com/a/45389462
+      #
+      if ! grep -q 'set-option -g default-command' "$out/tmux.conf"; then
+        # NOTE: this code must not be included in the bashrc file! It writes to a file in /nix/store, so it will cause permission errors:
+        # https://nix.dev/manual/nix/2.23/installation/multi-user#multi-user-mode
+        echo "set-option -g default-command \"${pkgs.bashInteractive}/bin/bash --rcfile \"$out/bashrc\"\"" >> "$out/tmux.conf"
+      fi
+
+      # Dynamically writes more content to our custom bashrc file
       cat <<EOT >> $out/bashrc
 
-# Configure tmux to use an interactive non-login shell then have it source our custom bashrc file and use our custom configs:
-#  - https://discourse.nixos.org/t/interactive-bash-with-nix-develop-flake/15486
-#  - https://unix.stackexchange.com/a/541352
-#  - https://stackoverflow.com/a/45389462
+# Configures tmux to use our custom configs:
+#  - https://unix.stackexchange.com/a/663023
+#  - https://askubuntu.com/a/746846
 #
-if ! grep -q 'set-option -g default-command' "$out/tmux.conf"; then
-  echo "set-option -g default-command \"${pkgs.bashInteractive}/bin/bash --rcfile \"$out/bashrc\"\"" >> "$out/tmux.conf"
-fi
 alias tmux="tmux -u -f \"$out/tmux.conf\""
 
-# Configure neovim to use our custom configs:
+# Configures neovim to use our custom configs:
+#  - https://vi.stackexchange.com/questions/37639/viminit-conflicts-for-neovim-and-vim
 #  - https://neovim.io/doc/user/starting.html#initialization
 #  - https://stackoverflow.com/a/75633317
-#  - https://vi.stackexchange.com/questions/37639/viminit-conflicts-for-neovim-and-vim
 #
 alias nvim="XDG_CONFIG_HOME=\"$out\" NVIM_APPNAME=\"nvim\" nvim"
 
-# Configure starship to use our custom configs
+# Configures starship to use our custom configs
 export STARSHIP_CONFIG="$out/starship.toml"
 eval "\$(starship init bash)"
 EOT
