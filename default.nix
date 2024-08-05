607f929a7a4bc6f86ebdd364b91a46a798decae6
chris-de-leon <chrisdeleon333+dev@gmail.com>
2024-08-04 17:34:39 -0700

    refactoring setup and fixing bug where starship prompt gets reset in nested nix shells

--- default.nix
+++ default.nix
@@ -1,77 +1,45 @@
-{ pkgs ? import<nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
-# https://github.com/samdroid-apps/nix-articles/blob/master/04-proper-mkderivation.md#using-the-content
 let
   inherit (pkgs) stdenv;
-in
-let
-  content = stdenv.mkDerivation {
-    name = "dotfiles";
-    src = ./configs;
-    installPhase = ''
-      # Exits early if any command runs into an issue and prints commands as they are executed
-      set -ex
-
-      # Copies source files into an output folder
-      mkdir -p $out
-      cp -rv $src/* $out
-
-      # Grants elevated permissions to tmux.conf and bashrc. In the end, these 
-      # will be given 555 permissions: https://stackoverflow.com/a/61736179
-      chmod 777 $out/tmux.conf
-      chmod 777 $out/bashrc
-      
-      # Configures tmux to use an interactive non-login shell and source our custom bashrc file:
-      #  - https://discourse.nixos.org/t/interactive-bash-with-nix-develop-flake/15486
-      # - https://unix.stackexchange.com/a/98085
-      #  - https://unix.stackexchange.com/a/541352
-      #  - https://stackoverflow.com/a/45389462
-      #
-      if ! grep -q 'set-option -g default-command' "$out/tmux.conf"; then
-        # NOTE: this code must not be included in the bashrc file! It writes to a file in /nix/store, so it will cause permission errors:
-        # https://nix.dev/manual/nix/2.23/installation/multi-user#multi-user-mode
-        echo "set-option -g default-command \"${pkgs.bashInteractive}/bin/bash --rcfile \"$out/bashrc\"\"" >> "$out/tmux.conf"
-      fi
-
-      # Dynamically writes more content to our custom bashrc file
-      cat <<EOT >> $out/bashrc
-
-# Configures tmux to use our custom configs:
-#  - https://unix.stackexchange.com/a/663023
-#  - https://askubuntu.com/a/746846
-#
-alias tmux="tmux -u -f \"$out/tmux.conf\""
-
-# Configures neovim to use our custom configs:
-#  - https://vi.stackexchange.com/questions/37639/viminit-conflicts-for-neovim-and-vim
-#  - https://neovim.io/doc/user/starting.html#initialization
-#  - https://stackoverflow.com/a/75633317
-#
-alias nvim="XDG_CONFIG_HOME=\"$out\" NVIM_APPNAME=\"nvim\" nvim"
-
-# Configures starship to use our custom configs
-export STARSHIP_CONFIG="$out/starship.toml"
-eval "\$(starship init bash)"
-EOT
-    '';
-  };
+  bashrc = pkgs.writeText "bashrc" ''
+    if [ -f "$HOME/.bashrc" ]; then
+      . "$HOME/.bashrc"
+    fi
+  '';
 in
 stdenv.mkDerivation {
-  name = "dotfiles-shell";
-  buildInputs = [
-    pkgs.starship
-    pkgs.unzipNLS
-    pkgs.ripgrep
-    pkgs.lazygit
-    pkgs.neovim
-    pkgs.tmux
-    pkgs.vim
-    pkgs.fd
-    pkgs.jq
-  ];
-  shellHook = ''
-    export DOTFILES_BASHRC=${content}/bashrc
-    export DOTFILES_HOME=${content}
-    . ${content}/bashrc
+  name = "dotfiles";
+  src = ./configs;
+  installPhase = ''
+    # Exits early if any command runs into an issue and prints commands as they are executed
+    set -ex
+
+    # Copies source files into an output folder
+    mkdir -p $out
+    cp -v ${bashrc} $out/bashrc
+    cp -rv $src/* $out
+
+    # Grants elevated permissions to tmux.conf and bashrc. In the end, these
+    # will be given 555 permissions: https://stackoverflow.com/a/61736179
+    chmod 777 $out/tmux.conf
+    chmod 777 $out/bashrc
+
+    # Add starship setup commands to bashrc
+    printf "\nexport STARSHIP_CONFIG=\"$out/starship.toml\" && eval \"\$(starship init bash)\"" >> "$out/bashrc"
+
+    # Configures tmux to use an interactive non-login shell and source our custom bashrc file:
+    #
+    #  - https://discourse.nixos.org/t/interactive-bash-with-nix-develop-flake/15486
+    #  - https://unix.stackexchange.com/a/98085
+    #  - https://unix.stackexchange.com/a/541352
+    #  - https://stackoverflow.com/a/45389462
+    #
+    # This code must not be included in the bashrc file! It writes to the tmux config file in /nix/store, so
+    # it will cause permission errors: https://nix.dev/manual/nix/2.23/installation/multi-user#multi-user-mode
+    #
+    if ! grep -q 'set-option -g default-command' "$out/tmux.conf"; then
+      echo "set-option -g default-command \"${pkgs.bashInteractive}/bin/bash --rcfile \"$out/bashrc\"\"" >> "$out/tmux.conf"
+    fi
   '';
 }
--- flake.nix
+++ flake.nix
@@ -6,16 +6,91 @@
   };
 
   outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem(system:
+    utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
         dotfiles = import ./default.nix { inherit pkgs; };
-      in {
-        defaultPackage = dotfiles;
+      in
+      rec {
+        formatter = pkgs.nixpkgs-fmt;
+
+        # Configures tmux to use our custom configs:
+        #  - https://unix.stackexchange.com/a/663023
+        #  - https://askubuntu.com/a/746846
+        #
+        wrappedTmux = pkgs.symlinkJoin {
+          name = "tmux";
+          paths = [ pkgs.tmux ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/tmux \
+              --add-flags "-u" \
+              --add-flags "-f" \
+              --add-flags "${dotfiles}/tmux.conf"
+          '';
+        };
+
+        # Configures neovim to use our custom configs:
+        #  - https://vi.stackexchange.com/questions/37639/viminit-conflicts-for-neovim-and-vim
+        #  - https://neovim.io/doc/user/starting.html#initialization
+        #  - https://stackoverflow.com/a/75633317
+        #
+        wrappedNvim = pkgs.symlinkJoin {
+          name = "nvim";
+          paths = [ pkgs.neovim ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/nvim \
+              --set "XDG_CONFIG_HOME" "${dotfiles}" \
+              --set "NVIM_APPNAME" "nvim"
+          '';
+        };
 
         devShell = pkgs.mkShell {
-          buildInputs = dotfiles.buildInputs;
-          shellHook = dotfiles.shellHook;
+          # NOTE: if you are already in a `nix develop` shell session and you start a new nested nix shell with
+          # `nix develop`, then Nix will reset PS1 and remove any unexported functions that were defined in your
+          # previous shell. This is problematic because if our previous shell was styled using let's say starship
+          # for example, then any shell styling that starship provided would be reset in the nested shell, which
+          # is not ideal. To fix this, we can use the PROMPT_COMMAND env variable, which doesn't seem to be reset
+          # by Nix:
+          #
+          #   https://github.com/NixOS/nix/issues/9174#issuecomment-1769023518
+          #
+          # Initially, we'll configure PROMPT_COMMAND such that it sources our custom bashrc file, which includes the
+          # steps needed to configure starship. When we call nix develop for the first time, the PROMPT_COMMAND won't
+          # activate immediately (since we haven't run any commands yet), so we also need to source the custom bashrc
+          # file in the shellHook so that it is styled once we enter it. When the first command is run in the shell,
+          # the PROMPT_COMMAND will get evaluated and it will source the bashrc file. This causes the starship setup
+          # commands to be evaluated, which results in PROMPT_COMMAND getting reassigned to one of starship's helper
+          # functions. At that point, everything should be working in the current shell. However, to ensure that this
+          # styling is maintained for future shells, we need to ensure that the starship function that PROMPT_COMMAND
+          # was assigned to (and all the starship helper functions it references) are exported for future shells. To
+          # do this, we use `set -a` and `set +a` so that all the functions we need are exported properly.
+          #
+          #   https://unix.stackexchange.com/a/430690
+          #
+          PROMPT_COMMAND = "set -a; . ${dotfiles}/bashrc; set +a;";
+
+          # Export the location of the dotfile configs
+          DOTFILES_HOME = "${dotfiles}";
+
+          # Add common dev tools
+          packages = [
+            pkgs.starship
+            pkgs.unzipNLS
+            pkgs.ripgrep
+            pkgs.lazygit
+            wrappedNvim
+            wrappedTmux
+            pkgs.vim
+            pkgs.fd
+            pkgs.jq
+          ];
+
+          # Source the custom bashrc file
+          shellHook = ''
+            . ${dotfiles}/bashrc
+          '';
         };
       }
     );
