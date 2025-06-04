0fd086acfa3128619798f453f38a9010f869d95e
chris-de-leon <135092418+chris-de-leon@users.noreply.github.com>
2025-06-04 13:45:05 -0700

    fix: install script idempotency improvements and profile directory check

--- workspace/scripts/install.sh
+++ workspace/scripts/install.sh
@@ -2,16 +2,23 @@
 
 set -eo pipefail
 
-# Repo details
+# Helper Vars
+NIX_DAEMON="/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh"
 REPO_OWNER="chris-de-leon"
 REPO_NAME="dotfiles"
 
-# Install Nix if it isn't already installed
+# If Nix doesn't exist, then try to source the daemon first
+if ! command -v nix &>/dev/null && [[ -f "${NIX_DAEMON}" ]]; then
+  # shellcheck source=/dev/null
+  . "${NIX_DAEMON}"
+fi
+
+# If Nix still isn't available, then install it
 if ! command -v nix &>/dev/null; then
   echo "info: installing Nix..."
-  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+  curl --proto '=https' --tlsv1.2 -sSf -L 'https://install.determinate.systems/nix/tag/v3.6.2' | sh -s -- install --no-confirm
   # shellcheck source=/dev/null
-  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+  . "${NIX_DAEMON}"
 else
   echo "info: Nix is already installed"
 fi
@@ -23,6 +30,8 @@ echo "info: ${NIX_VERSION}"
 # Use Nix to install dev tools
 NIX_PROFILE_URL="git+https://github.com/${REPO_OWNER}/${REPO_NAME}.git"
 NIX_PROFILE_LOC="${HOME}/.nix-profile/profile.devenv"
+NIX_PROFILE_DIR="$(readlink "${HOME}/.nix-profile")"
+mkdir -p "${NIX_PROFILE_DIR}"
 if nix profile list --no-pretty | grep -q "${NIX_PROFILE_URL}"; then
   echo "info: upgrading dev tools..."
   nix profile upgrade --profile "${NIX_PROFILE_LOC}" --all
@@ -36,12 +45,12 @@ fi
 # Get user credentials
 if [[ "${DOTFILES_RECIPE:-}" == "secrets" ]]; then
   if [[ -z "${LASTPASS_USERNAME:-}" ]]; then
-    read -rp "info: please enter your LastPass username: " LASTPASS_USERNAME </dev/tty && echo ""
+    read -rp "info: please enter your LastPass username: " LASTPASS_USERNAME && echo ""
     lpass login --trust "${LASTPASS_USERNAME}"
   fi
 else
   if [[ -z "${GITHUB_TOKEN:-}" ]]; then
-    read -rps "info: please enter your GitHub PAT: " GITHUB_TOKEN </dev/tty && echo ""
+    read -rps "info: please enter your GitHub PAT: " GITHUB_TOKEN && echo ""
     export GITHUB_TOKEN="${GITHUB_TOKEN}"
   fi
 fi
